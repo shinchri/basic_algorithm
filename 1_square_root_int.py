1937c0cf8c7a26a9063d5e14d53115f696d43a7c
Chris Shin <17078706+shinchri@users.noreply.github.com>
2021-10-16 15:20:36 -0500

    chore: Complete problem 1

--- 1_square_root_int.py
+++ 1_square_root_int.py
@@ -9,6 +9,38 @@ If the given number if 27, the answer would be 5 because sqrt(5) = 5.196 whose f
 
 The expected time complexity is `O(log(n))`
 """
+def floor(number):
+
+    return number - number%1
+"""
+i < j
+"""
+def sqrt_recursive(number, i, j):
+    i = floor(i)
+    j = floor(j)
+
+    if j*j == number:
+        return j
+
+    if i*i == number:
+        return i
+
+    if number > j*j:
+        return j
+
+    if number > i*i and number < j*j:
+        mid = floor((j+i)/2)
+        if mid*mid == number:
+            return mid
+        
+        if mid*mid > number and mid*mid < number:
+            output = sqrt_recursive(number, mid, j-1)
+        
+        else:
+            output = sqrt_recursive(number, i+1, mid)
+    elif number < i*i:
+        output = sqrt_recursive(number, i/2, i-1)
+    return output
 
 def sqrt(number):
     """
@@ -18,12 +50,23 @@ def sqrt(number):
        number(int): Number to find the floored squared root
     Returns:
        int: Floored Square Root
-    """
-    pass
+    """    
+    if number == 0:
+        return 0
+
+    if number <= 3:
+        return 1
+
+    return sqrt_recursive(number, number/4, number/2)
+
+
+
 
 print ("Pass" if  (3 == sqrt(9)) else "Fail")
 print ("Pass" if  (0 == sqrt(0)) else "Fail")
 print ("Pass" if  (4 == sqrt(16)) else "Fail")
 print ("Pass" if  (1 == sqrt(1)) else "Fail")
 print ("Pass" if  (5 == sqrt(27)) else "Fail")
-
+print ("Pass" if  (6 == sqrt(36)) else "Fail")
+print ("Pass" if  (6 == sqrt(37)) else "Fail")
+print ("Pass" if  (5 == sqrt(35)) else "Fail")
