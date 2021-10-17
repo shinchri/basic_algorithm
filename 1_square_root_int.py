f86bbe7c09ed9b69d638e91c7dc057a6c93e6d46
Chris Shin <17078706+shinchri@users.noreply.github.com>
2021-10-16 22:09:14 -0500

    test: Add test case for problem 1

--- 1_square_root_int.py
+++ 1_square_root_int.py
@@ -50,7 +50,10 @@ def sqrt(number):
        number(int): Number to find the floored squared root
     Returns:
        int: Floored Square Root
-    """    
+    """
+    if number < 0:
+        return None
+
     if number == 0:
         return 0
 
@@ -60,8 +63,10 @@ def sqrt(number):
     return sqrt_recursive(number, number/4, number/2)
 
 
+### TEST CASES ###
 
-
+# prints "Pass"
+print ("Pass" if  (None == sqrt(-1)) else "Fail")
 print ("Pass" if  (3 == sqrt(9)) else "Fail")
 print ("Pass" if  (0 == sqrt(0)) else "Fail")
 print ("Pass" if  (4 == sqrt(16)) else "Fail")
